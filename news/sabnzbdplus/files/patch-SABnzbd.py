0aecbe521ff964ac4c42971a9cdc446aadb464a7
Jonathan Walker <kallous@gmail.com>
2011-07-31 13:33:35 -0500

    reverted most changes as a user script was the appropriate fix

--- news/sabnzbdplus/files/patch-SABnzbd.py
+++ news/sabnzbdplus/files/patch-SABnzbd.py
@@ -1,9 +1,5 @@
-@@ -1,4 +1,4 @@
--#!/usr/bin/python -OO
-+#!/usr/local/bin/python2.6 -OO
- # Copyright 2008-2011 The SABnzbd-Team <team@sabnzbd.org>
- #
- # This program is free software; you can redistribute it and/or
+--- SABnzbd.py	2011-06-07 18:02:35.000000000 -0400
++++ SABnzbd-port.py	2011-06-08 07:20:14.948254196 -0400
 @@ -276,9 +276,6 @@
  
      os.chdir(sabnzbd.DIR_PROG)
@@ -29,7 +25,7 @@
      sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sabnzbd.MY_FULLNAME))
      sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
 -    sabnzbd.DIR_PROG = os.path.dirname(sabnzbd.MY_FULLNAME)
-+    sabnzbd.DIR_PROG = '/usr/local/share/sabnzbdplus'
++    sabnzbd.DIR_PROG = %%PREFIX%%
      sabnzbd.DIR_INTERFACES = real_path(sabnzbd.DIR_PROG, DEF_INTERFACES)
      sabnzbd.DIR_LANGUAGE = real_path(sabnzbd.DIR_PROG, DEF_LANGUAGE)
      org_dir = os.getcwd()
