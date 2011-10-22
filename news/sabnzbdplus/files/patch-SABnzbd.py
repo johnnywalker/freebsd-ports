99605c556a0ee75d2d560362fde7b7cb23c09ab4
Jonathan Walker <kallous@gmail.com>
2011-10-22 17:29:13 -0500

    Adjusted patch to work cleanly with 0.6.9

--- news/sabnzbdplus/files/patch-SABnzbd.py
+++ news/sabnzbdplus/files/patch-SABnzbd.py
@@ -1,15 +1,5 @@
---- SABnzbd.py	2011-06-07 18:02:35.000000000 -0400
-+++ SABnzbd-port.py	2011-06-08 07:20:14.948254196 -0400
-@@ -276,9 +276,6 @@
- 
-     os.chdir(sabnzbd.DIR_PROG)
-     os.setsid()
--    # Make sure I can read my own files and shut out others
--    prev= os.umask(0)
--    os.umask(prev and int('077',8))
- 
-     try:
-         pid = os.fork()
+--- SABnzbd.py.orig	2011-09-10 04:04:42.000000000 -0500
++++ SABnzbd.py	2011-10-22 17:27:00.712599406 -0500
 @@ -40,6 +40,9 @@
      print "The Python module Cheetah is required"
      sys.exit(1)
@@ -20,7 +10,17 @@
  import cherrypy
  if not cherrypy.__version__.startswith("3.2"):
      print "Sorry, requires Python module Cherrypy 3.2 (use the included version)"
-@@ -932,7 +935,7 @@
+@@ -278,9 +281,6 @@
+ 
+     os.chdir(sabnzbd.DIR_PROG)
+     os.setsid()
+-    # Make sure I can read my own files and shut out others
+-    prev= os.umask(0)
+-    os.umask(prev and int('077',8))
+ 
+     try:
+         pid = os.fork()
+@@ -932,7 +932,7 @@
  
      sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sabnzbd.MY_FULLNAME))
      sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
